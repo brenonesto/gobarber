ccdc07ed4ba193521257690246e238dcbdfc228a
Breno Nesto <breno.nesto@hotmail.com>
2020-02-04 00:49:36 -0300

    Adding the file upload, appointments and notifications

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'baafccc57e7b4c',
+    pass: '19913deb38d4a3',
+  },
+  default: {
+    from: 'Equipe GoBarber  <noreply@gobarber.com>',
+  },
+};
